72f98fb272321724ae91ca6d4d18dbd71a48a006
Narin <narin.tana@hotmail.com>
2023-03-08 21:23:32 +0000

    Show zero cost dice

--- components/CardComponent.vue
+++ components/CardComponent.vue
@@ -57,7 +57,7 @@
                                 class="flex flex-row"
                             >
                                 <div
-                                    v-for="point of card.skills.find(skill => skill.name === 'Description').points.filter(point => point.point_count > 0)"
+                                    v-for="point of card.skills.find(skill => skill.name === 'Description').points"
                                     :key="point.point_type"
                                     :style="`backgroundImage: url(${require(`~/assets/icons/cost/${point.point_type}.png`)}); text-shadow: -1px -1px 0 #333, 1px -1px 0 #333, -1px 1px 0 #333, 1px 1px 0 #333`"
                                     class="w-12 h-12 bg-center bg-contain flex justify-center items-center text-2xl shrink-0"
--- components/DeckList.vue
+++ components/DeckList.vue
@@ -10,7 +10,7 @@
                     class="font-genshin p-2 flex flex-row"
                 >
                     <div
-                        v-for="point of cardsData?.find(cardData => cardData.name === card)?.skills.find(skill => skill.name === 'Description')?.points.filter(point => point.point_count > 0)"
+                        v-for="point of cardsData?.find(cardData => cardData.name === card)?.skills.find(skill => skill.name === 'Description')?.points"
                         :key="point.point_type"
                         :style="`backgroundImage: url(${require(`~/assets/icons/cost/${point.point_type}.png`)}); text-shadow: -1px -1px 0 #333, 1px -1px 0 #333, -1px 1px 0 #333, 1px 1px 0 #333`"
                         class="w-12 h-12 bg-center bg-contain flex justify-center items-center text-2xl"
